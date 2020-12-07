b8bd2a4e767cd37283424498dd3f2a3156c84197
Alexander <alexanderjarl91@gmail.com>
2020-12-07 15:30:18 +0000

    mongodb

--- config/database.js
+++ config/database.js
@@ -1,14 +1,30 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "mongoose",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri:
+          "mongodb+srv://alexanderjarl:4PLP1h38Om58GocC@cluster0.gclk0.mongodb.net/strapidb?retryWrites=true&w=majority",
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
